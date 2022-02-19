eb42a35ea0e09d9d7b6c70ba4f43382fcb1bcec3
raihan <raihanmuhd97@gmail.com>
2022-02-19 13:45:53 +0800

    update export import to use * also

--- js-refresher/14_exports_and_imports/main_file.js
+++ js-refresher/14_exports_and_imports/main_file.js
@@ -5,7 +5,11 @@ import prs from './person.js'
 // can set alias if you want
 // can export multiple variables from a file
 import { printName as pn, baseValue } from './utils.js'
+import * as bundled from './utils.js'
 
 pn(prs.name);
 console.log(baseValue);
 
+// same results
+bundled.printName(prs.name);
+console.log(bundled.baseValue);
