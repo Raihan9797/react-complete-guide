2460e5c16db5ab6c1ad1839616337207c80d9aa8
raihan <raihanmuhd97@gmail.com>
2022-02-20 17:15:49 +0800

    keep section 4 for branch 4

--- section-2-js-refresher/14_exports_and_imports/person.js
+++
@@ -1,8 +0,0 @@
-// an object usually uses const to make
-// object properties no need semicolon
-const person = {
-    name : 'Max'
-}
-
-// this means that by default, it will auto export the person object
-export default person
