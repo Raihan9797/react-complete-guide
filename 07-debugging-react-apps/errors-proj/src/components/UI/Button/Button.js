d1a88d01e5ee8b0ab91ac3a224520858543e02ab
raihan <raihanmuhd97@gmail.com>
2022-02-22 15:55:13 +0800

    new project files for section 7

--- 07-debugging-react-apps/errors-proj/src/components/UI/Button/Button.js
+++ 07-debugging-react-apps/errors-proj/src/components/UI/Button/Button.js
@@ -3,7 +3,6 @@ import React from 'react';
 import styles from './Button.module.css';
 
 const Button = props => {
-  console.log(styles.button);
   return (
     <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
